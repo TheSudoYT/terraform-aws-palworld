287cbf9928ae1dfbdfd0910653e8755f4e9e6f6a
Joshua Tracy <55331456+Josh-Tracy@users.noreply.github.com>
2024-01-27 14:17:35 -0500

    fix(restore from backup): start_from_backup now enforces that a dedicated_server_name_hash be provided when true

--- modules/compute/variables.tf
+++ modules/compute/variables.tf
@@ -113,25 +113,6 @@ variable "backup_interval_cron_expression" {
   default     = "0 23 * * *"
 }
 
-# Backup inputs
-variable "create_backup_s3_bucket" {
-  description = "True or False. Do you want to create an S3 bucket to FTP backups into"
-  type        = bool
-  default     = false
-}
-
-variable "s3_bucket_backup_retention" {
-  description = "Lifecycle rule. The number of days to keep backups in S3 before they are deleted"
-  type        = number
-  default     = 7
-}
-
-variable "force_destroy" {
-  description = "True or False. Set to true if you want Terraform destroy commands to have the ability to destroy the backup bucket while it still containts backup files"
-  type        = bool
-  default     = false
-}
-
 ## Restore from backups
 variable "start_from_backup" {
   description = "True of False. Set true to start the server from an existing palworld save. Requires existing save game files."
